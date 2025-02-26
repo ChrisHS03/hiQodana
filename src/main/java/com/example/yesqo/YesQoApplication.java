b59fb510c92b877e2694995cba9327eecda1c825
chrisHS03 <chso0005@stud.kea.dk>
2025-02-26 10:04:45 +0100

    initial commit

---
+++ src/main/java/com/example/yesqo/YesQoApplication.java
@@ -0,0 +1,13 @@
+package com.example.yesqo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesQoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesQoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yesqo/YesQoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yesqo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesQoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
